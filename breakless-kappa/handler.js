2990aa1390893fae09b9bdfae32a82084b8662aa
Julio Faerman <jfaerman@gmail.com>
2016-12-19 13:28:36 -0200

    Validating jobID

--- breakless-kappa/handler.js
+++ breakless-kappa/handler.js
@@ -1,13 +1,17 @@
 'use strict';
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
+const assert = require('assert');
 
 module.exports.publishToS3 = (event, context, callback) => {
     let codepipeline = new AWS.CodePipeline();
     let job = event["CodePipeline.job"] || {};
     let jobId = job.id;
-    let eventStr = JSON.stringify(event);
+    assert(jobId,"CodePipeline Job ID is required but not found.");
+    let eventStr   = JSON.stringify(event);
+    let contextStr = JSON.stringify(context);
     console.log(eventStr);
-    console.log(context);
+    console.log("===");
+    console.log(contextStr);
 
     var putJobSuccess = function(message) {
         var params = {
